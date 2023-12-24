0614aae3e9dd77c4f812f68da2c790003e7d8b50
karthikeya-21 <kaarthikeyadeekonda@gmail.com>
2023-12-24 23:12:43 +0530

    files added

---
+++ server.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('A new client connected!');
+
+  // Send a welcome message to the client
+  ws.send('Welcome to the WebSocket server!');
+
+  // Handle incoming messages from the client
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle client disconnection
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3050;
+server.listen(PORT, () => {
+  console.log(`WebSocket server listening on port ${PORT}`);
+});
