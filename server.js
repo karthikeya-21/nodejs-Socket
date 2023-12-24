692380b41c580eb99d709d253da9bf028a022a27
karthikeya-21 <kaarthikeyadeekonda@gmail.com>
2023-12-25 00:02:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ wss.on('connection', (ws) => {
 
     // Broadcast the message to all connected clients
     wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     });
