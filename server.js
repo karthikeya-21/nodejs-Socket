2b85c3378e214fed174492e161a8d959f781e540
karthikeya-21 <kaarthikeyadeekonda@gmail.com>
2023-12-26 15:52:15 +0530

    updated

--- server.js
+++ server.js
@@ -1,40 +1,92 @@
 const WebSocket = require('ws');
 const http = require('http');
-
+const { MongoClient,ObjectId  } = require('mongodb');
+const url = 'mongodb+srv://root:root@cluster0.1ekprvq.mongodb.net/?retryWrites=true&w=majority'; // replace with your MongoDB server URL
+const dbName = 'sockets';
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
+const { v4: uuidv4 } = require('uuid');
 
-wss.on('connection', (ws) => {
-  console.log('A new client connected!');
+async function getCollection(){
+  try{
+  const client=new MongoClient(url);
+  client.connect();
+  return client.db('sockets').collection('users');
+  }catch(err){
+      console.error(err);
+  }
+}
 
-  // Send a welcome message to the client
-  const welcomeMessage = {
-    type: 'welcome',
-    content: 'Welcome to the WebSocket server!'
-  };
-  ws.send(JSON.stringify(welcomeMessage));
+async function getUsersWithoutMe(excludedUserId) {
+  const userCollection = await getCollection();
 
-  // Handle incoming messages from the client
-  ws.on('message', (message) => {
+  try {
+    // Fetch all users except the one with the specified ID
+    const users = await userCollection.find(
+      { name: { $ne: new ObjectId(excludedUserId) } },
+      { projection: { _id: 1, name: 1 } }
+    ).toArray();
 
-    // Parse the received JSON data
-    try {
-      const jsonData = JSON.parse(message);
+    return users;
+  } catch (error) {
+    console.error('Error fetching users', error);
+    return [];
+  }
+}
 
-      // Broadcast the JSON data to all connected clients
-      wss.clients.forEach((client) => {
-        if (client!=ws && client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(jsonData));
-        }
-      });
+
+const socketMap = new Map();
+
+wss.on('connection', async (ws,req) => {
+  const username = req.url.replace('/?username=', '');
+  const userCollection = await getCollection();
+  const socketId = uuidv4();
+  socketMap.set(socketId, ws);
+  try {
+    await userCollection.updateOne({ name: username }, { $set: { connId: socketId } }, { upsert: true });
+  } catch (error) {
+    console.error('Error updating user');
+  }
+  ws.send(JSON.stringify({ type: 'connection',data:'Welcome to WebSockets'}));
+  // let users =await userCollection.find().toArray() ;
+  // const data={type:'userList',data:users};
+  // ws.send(JSON.stringify(data));
+  ws.on('message',async (message) => {
+try {
+      const data = JSON.parse(message);
+      console.log(data);
+      //If a message is recieived
+      if(data.type=='message'){
+      const recipientId = data.id;
+      const messageContent = data.data;
+
+      const recipient = await userCollection.findOne({ _id: new ObjectId(recipientId) });
+      if (recipient && recipient.connId) {
+        const recipientSocket = socketMap.get(recipient.connId);
+        recipientSocket.send(JSON.stringify({
+          type: 'message',
+          username:username,
+          data: `${messageContent}`,
+        }));
+      } else {
+        console.error('Recipient not found or not connected');
+      }
+    }
+    //send all the users
+    if(data.type=='userList'){
+      const users = await getUsersWithoutMe(data.name);
+      const data={type:'userList',data:users};
+      ws.send(JSON.stringify(data));
+    }
+
+
     } catch (error) {
-      console.error('Error parsing JSON:', error);
+      console.error('Error processing message:', error);
     }
   });
 
-  // Handle client disconnection
   ws.on('close', () => {
-    console.log('Client disconnected');
+
   });
 });
 
